4a80560eefb68291d9d184ba8f108fd682e78df8
erikkaugusto <erik.ahlert@universo.univates.br>
2024-08-21 09:22:19 -0300

    criando novos elementos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,10 +1,29 @@
 async function listaVideos() {
-    const conexao = await fetch("http://localhost:3000/videos"); // requisição GET, AWAIT é esperar a promise ser resolvida
+    const conexao = await fetch("http://localhost:3000/videos"); // requisição GET (se for outro tem que especificar como na linha 10), AWAIT para esperar a promise ser resolvida
     const conexaoConvertida = await conexao.json();
    
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
