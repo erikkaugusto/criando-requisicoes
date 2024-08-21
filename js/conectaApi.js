d2f092616ade36e7604448d744763686e36bebcf
erikkaugusto <erik.ahlert@universo.univates.br>
2024-08-21 10:43:50 -0300

    buscando elementos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,11 +1,11 @@
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos"); // requisição GET (se for outro tem que especificar como na linha 10), AWAIT para esperar a promise ser resolvida
     const conexaoConvertida = await conexao.json();
-   
+
     return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, url, imagem){
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
@@ -23,7 +23,15 @@ async function criaVideo(titulo, descricao, url, imagem){
     return conexaoConvertida;
 }
 
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
