ff3da51ce4bcb83a37d9bd943d08734f8bb5ab6f
erikkaugusto <erik.ahlert@universo.univates.br>
2024-08-24 15:22:53 -0300

    customizando erros

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,7 +1,7 @@
 import { conectaApi } from "./conectaApi.js";
 const formulario = document.querySelector("[data-formulario]");
 
-async function criarVideo(evento){
+async function criarVideo(evento) {
     evento.preventDefault();
 
     const imagem = document.querySelector("[data-imagem]").value;
@@ -9,8 +9,12 @@ async function criarVideo(evento){
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
 
-    await conectaApi.criaVideo(titulo, descricao, url, imagem);
-    window.location.href = "../pages/envio-concluido.html";
+    try {
+        await conectaApi.criaVideo(titulo, descricao, url, imagem);
+        window.location.href = "../pages/envio-concluido.html";
+    } catch (e) {
+        alert(e);
+    }
 }
 
 formulario.addEventListener("submit", evento => criarVideo(evento))
